ff465a75836ff1a4996209a4da5dcc3c948f4fcc
crashbox-hub <61722658+crashbox-hub@users.noreply.github.com>
2023-07-31 16:17:31 +1000

    Started file structure and downloaded required data.
    Pulled Artefacts from online repo and moved to seperate directory.

--- data_model.py
+++ data_model.py
@@ -1,6 +1,14 @@
 # The data model for the relevant data
+#
 
-# Read in the data from the csv file in folder "Crash Statistics Victoria.csv""
+# Import pandas to read in the data
+import pandas as pd
+
+# Read in the data from the csv file in folder "Crash Statistics Victoria.csv"
+data_set = pd.read_csv("Crash Statistics Victoria.csv")
+
+# Create a new data frame with only the relevant data
+
 
 
 
