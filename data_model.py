bb64d5f86049258aeaeb07ed62470bb045a4c81f
crashbox-hub <61722658+crashbox-hub@users.noreply.github.com>
2023-07-31 14:58:17 +1000

    Started file structure and downloaded required data.

---
+++ data_model.py
@@ -0,0 +1,6 @@
+# The data model for the relevant data
+
+# Read in the data from the csv file in folder "Crash Statistics Victoria.csv""
+
+
+
---
+++ display_model.py
@@ -0,0 +1,13 @@
+# The model to display the data for the user.
+
+"""" Display Requirements:
+
+For a user-selected period, display the information of all accidents that happened in the period.
+For a user-selected period, produce a chart to show the number of accidents in each hour of the day (on average).
+For a user-selected period, retrieve all accidents caused by an accident type that contains a keyword (user entered), e.g. collision, pedestrian.
+Allow the user to analyze the impact of alcohol in accidents – ie: trends over time, accident types involving alcohol, etc.
+One other ‘insight’ or analysis tool of your choice
+
+"""
+
+
