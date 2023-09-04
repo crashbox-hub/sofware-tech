cf77b60f5d9ff78a4abf32b4733951ac65882bca
crashbox-hub <61722658+crashbox-hub@users.noreply.github.com>
2023-09-04 13:49:44 +1000

    display_model.py
    Changed from a single frame (panel)  -> notebook

--- display_model.py
+++ display_model.py
@@ -1,12 +1,17 @@
 # The model to display the data for the user.
 
-"""" Display Requirements:
+"""
+
+Display Requirements:
 
 For a user-selected period, display the information of all accidents that happened in the period.
 For a user-selected period, produce a chart to show the number of accidents in each hour of the day (on average).
-For a user-selected period, retrieve all accidents caused by an accident type that contains a keyword (user entered), e.g. collision, pedestrian.
-Allow the user to analyze the impact of alcohol in accidents – ie: trends over time, accident types involving alcohol, etc.
-One other ‘insight’ or analysis tool of your choice
+For a user-selected period, retrieve all accidents caused by an accident type that contains a keyword (user entered),
+e.g. collision, pedestrian.
+
+Allow the user to analyze the impact of alcohol in accidents – ie: trends over time, accident types involving alcohol,
+etc.
+One other ‘insight’ or analysis tool of your choice - Data Visualisation
 
 """
 
@@ -15,14 +20,15 @@ import wx
 # Custom class from wc.Frame
 class MyFrame(wx.Frame):
     def __init__(self, parent, title):
-        super(MyFrame, self).__init__(parent, title=title, size=(300, 200))
+        super(MyFrame, self).__init__(parent, title=title, size=(500, 400))
+
         #Panel for the UI elements
-        panel = wx.Panel(self)
-        button = wx.Button(panel, label="Click Me!")
+        notebook = wx.Notebook(self)
+        button = wx.Button(notebook, label="Click Me!")
 
         sizer = wx.BoxSizer(wx.VERTICAL)
         sizer.Add(button, 0, wx.ALIGN_CENTER | wx.ALL, 10)
-        panel.SetSizer(sizer)
+        notebook.SetSizer(sizer)
 
         self.Bind(wx.EVT_BUTTON, self.on_button_click, button)
 
