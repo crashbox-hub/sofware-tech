fc908ed816163f24da763fe6383c62a39a22009b
crashbox-hub <61722658+crashbox-hub@users.noreply.github.com>
2023-09-04 13:24:41 +1000

    display_model.py
    Started the display model. Simple GUI with a clickable button and message displayed on_button_click

--- display_model.py
+++ display_model.py
@@ -10,4 +10,32 @@ One other ‘insight’ or analysis tool of your choice
 
 """
 
+import wx
+
+class MyFrame(wx.Frame):
+    def __init__(self, parent, title):
+        super(MyFrame, self).__init__(parent, title=title, size=(300, 200))
+
+        panel = wx.Panel(self)
+        button = wx.Button(panel, label="Click Me!")
+
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(button, 0, wx.ALIGN_CENTER | wx.ALL, 10)
+        panel.SetSizer(sizer)
+
+        self.Bind(wx.EVT_BUTTON, self.on_button_click, button)
+
+        self.Centre()
+        self.Show(True)
+
+    def on_button_click(self, event):
+        wx.MessageBox("Button clicked!")
+
+if __name__ == '__main__':
+    app = wx.App()
+    frame = MyFrame(None, "Basic wxPython UI")
+    app.MainLoop()
+
+
+
 
