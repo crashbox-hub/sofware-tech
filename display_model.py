101637784f6de7187d7b1f1332d82b6fb235be43
crashbox-hub <61722658+crashbox-hub@users.noreply.github.com>
2023-09-04 17:11:12 +1000

    display_model.py
    Working on calendar picker for date ranges.

--- display_model.py
+++ display_model.py
@@ -1,7 +1,6 @@
 # The model to display the data for the user.
 
 """
-
 Display Requirements:
 
 For a user-selected period, display the information of all accidents that happened in the period.
@@ -16,71 +15,65 @@ One other ‘insight’ or analysis tool of your choice - Data Visualisation
 """
 
 import wx
+import wx.adv
+import datetime
 
-# Custom class from wc.Frame
+BORDER = 5
+
+
+# Custom class from wx.Frame
 class MyFrame(wx.Frame):
     def __init__(self, parent, title):
-        super(MyFrame, self).__init__(parent, title=title, size=(500, 400))
+        super(MyFrame, self).__init__(parent, title=title, size=(800, 600))
+
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        self.SetMinSize(wx.Size(500, 500))
+        notebook = wx.Notebook(self)
 
         # notebook for multiple tabs
-        notebook = wx.Notebook(self)
-        sizer = wx.BoxSizer(wx.VERTICAL)
         notebook.SetSizer(sizer)
-
-        # button = wx.Button(notebook, label="Click Me!")
-
         home_panel = HomePanel(notebook)
         notebook.AddPage(home_panel, "Home")
-
-        accinfo_panel = AccInfoPanel(notebook)
-        notebook.AddPage(accinfo_panel, "Accident Info")
-
+        accident_info_panel = AccInfoPanel(notebook)
+        notebook.AddPage(accident_info_panel, "Accident Info")
         map_panel = MapPanel(notebook)
         notebook.AddPage(map_panel, "Map")
 
         self.Centre()
         self.Show(True)
 
-"""
-# For the on_button_click function
-        sizer.Add(button, 0, wx.ALIGN_CENTER | wx.ALL, 10)
-        self.Bind(wx.EVT_BUTTON, self.on_button_click, button)
-"""
-"""
-    # Function on_button_click
-    def on_button_click(self, event):
-        wx.MessageBox("Button clicked!")
-"""
 
-#Define HomePanel class for notebook
+# Define HomePanel class for notebook
 class HomePanel(wx.Panel):
     def __int__(self, parent):
         super(HomePanel, self).__init__(parent)
-        home_text = wx.StaticText(self, label="Home Tab Welcome Here!")
-        sizer = wx.BoxSizer(wx.VERTICAL)
 
-#Define AccInfoPanel class for notebook
+
+# Define AccInfoPanel class for notebook
 class AccInfoPanel(wx.Panel):
-    def __int__(self, parent):
-        super(AccInfoPanel, self).__init__(parent)
-        acc_info_text = wx.StaticText(self, label="Acc Info Tab Welcome Here!")
-        sizer = wx.BoxSizer(wx.VERTICAL)
+    def __int__(self, parent, *args, **kwargs):
+        super().__init__(parent, *args, **kwargs)
 
-#Define MapPanel class for notebook
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        # Headline Text
+        self.headline_text = wx.StaticText(self, style=wx.TE_CENTER & ~wx.TE_LEFT & ~wx.TE_RIGHT,
+                                           label="Test")
+        sizer.Add(self.headline_text, 0, wx.ALL | wx.EXPAND, 5)
+
+        # Calendar under the text
+        self.cal = wx.adv.CalendarCtrl(self, 10, wx.DateTime.Now())
+        sizer.Add(self.cal, 0, wx.ALL | wx.CENTER, 5)
+        self.SetSizer(sizer)
+
+
+# Define MapPanel class for notebook
 class MapPanel(wx.Panel):
     def __int__(self, parent):
         super(MapPanel, self).__init__(parent)
-        map_text = wx.StaticText(self, label="Home Tab Welcome Here!")
-        sizer = wx.BoxSizer(wx.VERTICAL)
-
 
 
 # Entry Point
 if __name__ == '__main__':
     app = wx.App()
-    frame = MyFrame(None, "Basic wxPython UI")
+    frame = MyFrame(None, "VSADS Visualisation Tool")
     app.MainLoop()
-
-
-
-
