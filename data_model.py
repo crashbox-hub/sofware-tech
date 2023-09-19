c1be70c4c4a7a6d5e644a156dd9c59ccc3717ab2
crashbox-hub <61722658+crashbox-hub@users.noreply.github.com>
2023-09-19 16:57:12 +1000

    data_model.py
    - Created the basic structure for the data model.
    - Imported SQLite3
    - Implamented the DataModel class

--- data_model.py
+++ data_model.py
@@ -1,14 +1,33 @@
-# The data model for the relevant data
-#
-
-# Import pandas to read in the data
+import sqlite3
 import pandas as pd
 
-# Read in the data from the csv file in folder "Crash Statistics Victoria.csv"
-data_set = pd.read_csv("Crash Statistics Victoria.csv")
 
-# Create a new data frame with only the relevant data
-data_set.readline()
+class DataModel:
+    def __init__(self, db_file_path):
+
+        self.db_file_path = db_file_path
+        self.connection = sqlite3.connect(db_file_path)
+
+    def get_relevant_data(self, query):
+        try:
+            # Pandas execute a SQL query and retrieve data as a DataFrame
+            relevant_data = pd.read_sql_query(query, self.connection)
+            return relevant_data
+        except Exception as e:
+            print(f"Error executing query: {e}")
+            return None
+
+    def close_connection(self):
+        self.connection.close()
 
 
+if __name__ == "__main__":
+    db_file_path = "vsads.db"
+    data_model = DataModel(db_file_path)
 
+    # Example Query !!! Change once Middle Layer is implemented
+    query = "SELECT * FROM your_table WHERE your_condition"
+
+    # Access relevant data by executing the query
+    relevant_data = data_model.get_relevant_data(query)
+    data_model.close_connection()
