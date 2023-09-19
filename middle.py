ee778600600bd144faf793c9773ba37502c3ed80
crashbox-hub <61722658+crashbox-hub@users.noreply.github.com>
2023-09-19 17:03:40 +1000

    middle.py
    - Created the middle.py file and started connecting it

---
+++ middle.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from data_model import DataModel
+
+# A function to get a list of the desired accidents based on the keywords and filters from the display model
+# Using Guaruv's example from Design Document
+def get_accident_list()
+
+
