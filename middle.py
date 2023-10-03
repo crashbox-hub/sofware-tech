c2a5f52d86c7f3f6d1ce4cb96dbf5d86dd2db1d8
gauruv-grover <gauruvgrover@gmail.com>
2023-10-04 09:10:01 +1000

    commented switch case

--- middle.py
+++ middle.py
@@ -5,12 +5,10 @@
 # The data model is not aware of the display model
 # Use a switch statement to determine which method to call in the data model
 
-import numpy as np
 import pandas as pd
 import wx
 import wx.adv
 from matplotlib import pyplot as plt
-from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg
 
 import data_model
 
@@ -20,28 +18,22 @@ class DataProcessor:
         self.db_filename = db_filename
 
     # A switch statement to determine which method to call from the data model
-    def process_data(self, method, *args):
-        from data_model import (count_vsads_by_date_range,
-                                calculate_average_by_hour_of_day, filter_vsads_by_keywords,
-                                filter_vsads_by_alcohol)
-
-        #
-        # from display_model import class
-        if method == "count_vsads_by_date_range":
-            return count_vsads_by_date_range(*args)
-        elif method == "calculate_average_by_hour_of_day":
-            return calculate_average_by_hour_of_day()
-        elif method == "filter_vsads_by_keywords":
-            return filter_vsads_by_keywords(*args)
-        elif method == "filter_vsads_by_alcohol":
-            return filter_vsads_by_alcohol(*args)
-        else:
-            return "Invalid method"
-            # cursor.close()
+    # def process_data(self, method, *args):
+    #     if method == "count_vsads_by_date_range":
+    #         return data_model.count_vsads_by_date_range(*args)
+    #     elif method == "calculate_average_by_hour_of_day":
+    #         return data_model.calculate_average_by_hour_of_day()
+    #     elif method == "filter_vsads_by_keywords":
+    #         return data_model.filter_vsads_by_keywords(*args)
+    #     elif method == "filter_vsads_by_alcohol":
+    #         return data_model.filter_vsads_by_alcohol(*args)
+    #     else:
+    #         return "Invalid method"
+    #         # cursor.close()
 
     # A bar chart that displays the number of accidents by accident type for the parameters selected
     def TypeOfAccidentBarChart(self, keywords):
-        data = self.process_data("filter_vsads_by_keywords", keywords)
+        data = data_model.filter_vsads_by_keywords(keywords)
         df = pd.DataFrame(data, columns=['ACCIDENT_TYPE', 'INJ_OR_FATAL'])
         df = df.groupby('ACCIDENT_TYPE')['INJ_OR_FATAL'].sum()
         df.plot.bar()
@@ -49,7 +41,7 @@ class DataProcessor:
 
     # A Heatmap that displays the number of accidents by hour of day for the parameters selected
     def HourOfDayHeatmap(self, keywords):
-        data = self.process_data("filter_vsads_by_keywords", keywords)
+        data = data_model.filter_vsads_by_keywords(keywords)
         df = pd.DataFrame(data, columns=['ACCIDENT_TYPE', 'INJ_OR_FATAL', 'ACCIDENT_TIME'])
         df['ACCIDENT_TIME'] = pd.to_datetime(df['ACCIDENT_TIME'])
         df['hour'] = df['ACCIDENT_TIME'].dt.hour
@@ -59,7 +51,7 @@ class DataProcessor:
 
     # Create a scatter plot of the parameters selected against the longitude and latitude of the accidents
     def LocationScatterPlot(self, keywords):
-        data = self.process_data("filter_vsads_by_keywords", keywords)
+        data = data_model.filter_vsads_by_keywords(keywords)
         df = pd.DataFrame(data, columns=['ACCIDENT_TYPE', 'INJ_OR_FATAL', 'LONGITUDE', 'LATITUDE'])
         plt.scatter(df['LONGITUDE'], df['LATITUDE'])
         plt.show()
